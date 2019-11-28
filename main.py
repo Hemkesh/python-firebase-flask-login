bdcd2712390389cad1d37f54cc9dd14039385d18
Hemkesh Agrawal <agrawalh@msu.edu>
2019-11-27 19:06:50 -0500

    V1.0
    Initial Upload

---
+++ main.py
@@ -0,0 +1,118 @@
+################################################################################
+################################################################################
+########                                                                ########
+########   Python - Firebase - Flask Login/Register App                 ########
+########   Author: Hemkesh Agrawal                                      ########
+########   Website: http://hemkesh.com                                  ########
+########   Last updated on: 11/27/2019                                  ########
+########                                                                ########
+########   P.S. This is my first ever github project, so I              ########
+########   would love to hear your feedback : agrawalh@msu.edu          ########
+########                                                                ########
+################################################################################
+################################################################################
+
+import pyrebase
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
+
+app = Flask(__name__)       #Initialze flask constructor
+
+#Add your own details
+config = {
+  "apiKey": "PASTE_HERE",
+  "authDomain": "PASTE_HERE",
+  "databaseURL": "PASTE_HERE",
+  "storageBucket": "PASTE_HERE"
+}
+
+#initialize firebase
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
+
+#Initialze person as dictionary
+person = {"is_logged_in": False, "name": "", "email": "", "uid": ""}
+
+#Login
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+#Sign up/ Register
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+#Welcome page
+@app.route("/welcome")
+def welcome():
+    if person["is_logged_in"] == True:
+        return render_template("welcome.html", email = person["email"], name = person["name"])
+    else:
+        return redirect(url_for('login'))
+
+#If someone clicks on login, they are redirected to /result
+@app.route("/result", methods = ["POST", "GET"])
+def result():
+    if request.method == "POST":        #Only if data has been posted
+        result = request.form           #Get the data
+        email = result["email"]
+        password = result["pass"]
+        try:
+            #Try signing in the user with the given information
+            user = auth.sign_in_with_email_and_password(email, password)
+            #Insert the user data in the global person
+            global person
+            person["is_logged_in"] = True
+            person["email"] = user["email"]
+            person["uid"] = user["localId"]
+            #Get the name of the user
+            data = db.child("users").get()
+            person["name"] = data.val()[person["uid"]]["name"]
+            #Redirect to welcome page
+            return redirect(url_for('welcome'))
+        except:
+            #If there is any error, redirect back to login
+            return redirect(url_for('login'))
+    else:
+        if person["is_logged_in"] == True:
+            return redirect(url_for('welcome'))
+        else:
+            return redirect(url_for('login'))
+
+#If someone clicks on register, they are redirected to /register
+@app.route("/register", methods = ["POST", "GET"])
+def register():
+    if request.method == "POST":        #Only listen to POST
+        result = request.form           #Get the data submitted
+        email = result["email"]
+        password = result["pass"]
+        name = result["name"]
+        try:
+            #Try creating the user account using the provided data
+            auth.create_user_with_email_and_password(email, password)
+            #Login the user
+            user = auth.sign_in_with_email_and_password(email, password)
+            #Add data to global person
+            global person
+            person["is_logged_in"] = True
+            person["email"] = user["email"]
+            person["uid"] = user["localId"]
+            person["name"] = name
+            #Append data to the firebase realtime database
+            data = {"name": name, "email": email}
+            db.child("users").child(person["uid"]).set(data)
+            #Go to welcome page
+            return redirect(url_for('welcome'))
+        except:
+            #If there is any error, redirect to register
+            return redirect(url_for('register'))
+
+    else:
+        if person["is_logged_in"] == True:
+            return redirect(url_for('welcome'))
+        else:
+            return redirect(url_for('register'))
+
+if __name__ == "__main__":
+    app.run()
---
+++ readme.md
@@ -0,0 +1,33 @@
+#Python-Firebase-Flask login/register web-app
+
+## Features
+1) Login/Sign up - Hosted on firebase
+2) Fully responsive UI - Credits: https://codepen.io/danzawadzki/pen/EgqKRr
+3) Based on Python-Flask
+
+## Requirements
+```bash
+pip install pyrebase4
+pip install flask
+```
+
+## Setting up firebase
+
+1) Go to https://console.firebase.google.com
+2) Login/Register your account
+3) Click on add project
+4) Give project name
+5) Optional: select google analytics
+6) Create project
+7) Under "Get started by adding Firebase to your app", click on web app
+8) Name the web app and copy the "apiKey", "authDomain", "databaseURL", "storageBucket" from the code given there
+9) Go to main.py and add the values you copied above
+10) Go to console, click on authentication (On the left sidebar), click on sign-in method, and enable email/password sign in
+11) Go to Storage (On the left sidebar), and click on "Create Database", start in test mode for now, click done.
+12) You are all set on firebase !!!
+
+## Additional Info
+
+The server starts by default on http://127.0.0.1:5000/
+
+After successful sign-up, the user name, email and uid are stored in a global dictionary called person
